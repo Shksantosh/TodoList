59a4464102ddde94e5175cb5727847c4d21f1d45
shksantosh <santoshking82@gmail.com>
2023-12-13 13:03:48 +0530

    Redux ToolKit Using

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { useDispatch } from "react-redux"
 import AddTodo from "./components/AddTodo/AddTodo"
 import TodoList from "./components/TodoList/TodoList"
 import { bindActionCreators} from "redux"
-import { addTodo, deleteTodo, editTodo, todoFinished } from "./actions/todoActions";
-
+import todoReducer, { addTodo, deleteTodo, editTodo, todoFinished } from "./slices/todoSlice";
 function App() {
- 
   
   const dispatch = useDispatch()
   const actions = bindActionCreators({ addTodo, deleteTodo, editTodo, todoFinished }, dispatch)
